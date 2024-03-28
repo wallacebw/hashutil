e606f7bb49d34a91ed2b85f192f9d67994740b44
Brian Wallace <brian@wallacehouse.com>
2024-03-28 09:38:51 -0400

    added missing sha3_256

--- hash_generator.py
+++ hash_generator.py
@@ -73,7 +73,7 @@ SegmentResults = namedtuple('SegmentResults',[
 # define constants
 SUPPORTED_HASHES = [
     "sha1", "sha224", "sha256", "sha284", "sha512", #SHA
-    "sha3_224", "sha3_384", "sha3_512", # SHA3
+    "sha3_224", "sha3_256", "sha3_384", "sha3_512", # SHA3
     "blake2b", "blake2s", "md5"] # Misc
 SUPPORTED_ENCODINGS = [
     'ascii', 'latin_1', 'utf_8', # common
